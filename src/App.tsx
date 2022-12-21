0251b649e73a79a537724a71440c803f7d32549a
AnatolyNikol <fanka4ectba@mail.ru>
2022-12-21 22:40:36 +0500

    Add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./microtask 1/Header";
+import {Body} from "./microtask 1/Body";
+import {Footer} from "./microtask 1/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            {/*Microtask 1*/}
+            <Header />
+            <Body/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
