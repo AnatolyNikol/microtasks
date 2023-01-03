1f06802345aa7c2d99f4c841cf59c2e724efa300
AnatolyNikol <fanka4ectba@mail.ru>
2023-01-03 21:22:25 +0500

    Add topCars array and table

--- src/microtaks 2/NewComponent.tsx
+++ src/microtaks 2/NewComponent.tsx
@@ -10,14 +10,39 @@ type StudentType = {
     age: number
 }
 
-export const NewComponent = (props:NewComponentType) => {
+export const NewComponent = (props: NewComponentType) => {
+    const topCars = [
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'}
+    ]
+
     return (
-        <ul>
-            {props.students.map((objectFromStudentArray, index) => {
-                return (
-                    <li key={objectFromStudentArray.id}>{objectFromStudentArray.name}</li>
-                )
-            })}
-        </ul>
+        <>
+            <ul>
+                {props.students.map((objectFromStudentArray, index) => {
+                    return (
+                        <li key={objectFromStudentArray.id}>
+                            <span>{objectFromStudentArray.name}</span>
+                            <span> age: {objectFromStudentArray.age}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <table>
+                <tr>
+                    <th>Month</th>
+                    <th>Savings</th>
+                </tr>
+                <tr>
+                    <td>January</td>
+                    <td>$100</td>
+                </tr>
+                <tr>
+                    <td>February</td>
+                    <td>$80</td>
+                </tr>
+            </table>
+        </>
     )
 }
