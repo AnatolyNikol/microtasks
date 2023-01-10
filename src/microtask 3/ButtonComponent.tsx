6012aa36e64ab57bb40f8fe04fbb266fe850dfe3
AnatolyNikol <fanka4ectba@mail.ru>
2023-01-10 22:24:34 +0500

    Add myFirstSubscriber function

--- src/microtask 3/ButtonComponent.tsx
+++ src/microtask 3/ButtonComponent.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 export const ButtonComponent = () => {
+    const myFirstSubscriber = () => {
+        console.log('Hello I`m Vasya!')
+    }
+
     return (
         <>
-            <button onClick={() => {console.log('Hello')}}>MyYouTubeChanel-1</button>
+            {/*<button onClick={(event) => {console.log('Hello')}}>MyYouTubeChanel-1</button>*/}
+            <button onClick={myFirstSubscriber}>MyYouTubeChanel-1</button>
         </>
     )
 }
