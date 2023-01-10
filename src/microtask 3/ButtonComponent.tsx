7d0bbf5b11d0c3d835be79187eb118fae3018cbd
AnatolyNikol <fanka4ectba@mail.ru>
2023-01-10 22:52:50 +0500

    Add function exercise

--- src/microtask 3/ButtonComponent.tsx
+++ src/microtask 3/ButtonComponent.tsx
@@ -8,16 +8,30 @@ export const ButtonComponent = () => {
     //     console.log('Hello I`m Ivan!')
     // }
 
-    const onClickHandler = (name: string) => {
-        console.log(name)
+    // const onClickHandler = (name: string) => {
+    //     console.log(name)
+    // }
+
+    const func1 = () => {
+        //ничего не передавать, но в консоли должно показаться 100200
+        console.log(100200)
+    }
+
+    const func2 = (value: number) => {
+        //передать цифру 100200
+        console.log(value)
     }
 
     return (
         <>
             {/*<button onClick={(event) => {console.log('Hello')}}>MyYouTubeChanel-1</button>*/}
-            <button onClick={(event) => onClickHandler('Vasya')}>MyYouTubeChanel-1</button>
-            <button onClick={(event) => onClickHandler('Ivan')}>MyYouTubeChanel-2</button>
-            <button onClick={() => onClickHandler('some info')}>MyYouTubeChanel-3</button>
+
+            {/*<button onClick={(event) => onClickHandler('Vasya')}>MyYouTubeChanel-1</button>*/}
+            {/*<button onClick={(event) => onClickHandler('Ivan')}>MyYouTubeChanel-2</button>*/}
+            {/*<button onClick={() => onClickHandler('some info')}>MyYouTubeChanel-3</button>*/}
+
+            <button onClick={func1}>Button1</button>
+            <button onClick={() => func2(100200)}>Button2</button>
         </>
     )
 }
