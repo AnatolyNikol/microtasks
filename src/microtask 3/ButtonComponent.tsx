30d02110aa68b6f6f573c692486597799c4305ee
AnatolyNikol <fanka4ectba@mail.ru>
2023-01-10 22:43:29 +0500

    Add onClickHandler function

--- src/microtask 3/ButtonComponent.tsx
+++ src/microtask 3/ButtonComponent.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 
 export const ButtonComponent = () => {
-    const myFirstSubscriber = () => {
-        console.log('Hello I`m Vasya!')
+    // const myFirstSubscriber = () => {
+    //     console.log('Hello I`m Vasya!')
+    // }
+    // const mySecondSubscriber = () => {
+    //     console.log('Hello I`m Ivan!')
+    // }
+
+    const onClickHandler = (name: string) => {
+        console.log(name)
     }
 
     return (
         <>
             {/*<button onClick={(event) => {console.log('Hello')}}>MyYouTubeChanel-1</button>*/}
-            <button onClick={myFirstSubscriber}>MyYouTubeChanel-1</button>
+            <button onClick={(event) => onClickHandler('Vasya')}>MyYouTubeChanel-1</button>
+            <button onClick={(event) => onClickHandler('Ivan')}>MyYouTubeChanel-2</button>
+            <button onClick={() => onClickHandler('some info')}>MyYouTubeChanel-3</button>
         </>
     )
 }
