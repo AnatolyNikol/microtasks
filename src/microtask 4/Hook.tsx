d298f0c4acdaa1f52c1b4ae8e0faa308696494b4
AnatolyNikol <fanka4ectba@mail.ru>
2023-01-11 22:44:43 +0500

    Add onClick

--- src/microtask 4/Hook.tsx
+++ src/microtask 4/Hook.tsx
@@ -3,9 +3,15 @@ import React from "react";
 export const Hook = () => {
     let a = 1;
 
+    const onClickHandler = () => {
+        ++a;
+        console.log(a)
+    }
+
     return (
         <>
             <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
         </>
     )
 }
